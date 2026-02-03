9976aa2ea81e396de4cad11f57e5e8ae8484b3be
initishgrover <117805147+initishgrover@users.noreply.github.com>
2026-02-03 11:02:13 -0500

    UI and logic board is fixed and working

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UITableView.appearance().backgroundColor = .systemGray
+        UITableViewCell.appearance().backgroundColor = .systemYellow
         
         return true
     }
